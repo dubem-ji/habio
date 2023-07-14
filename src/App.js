a2ec9700ba9506825ecae683ab241316478e98f9
Joy Iroegbu <mapt.dubem@gmail.com>
2023-07-14 09:17:12 +0100

    clean react boilerplates

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          {/* <Route path='/' element={<Home />} /> */}
+        </Routes>
+        {/* <Navbar /> */}
+      </Router>
+    </>
+  );
+};
+
+export default App;
