ec0f0769956cebe34e55bcc8ff8fede29dfae067
Joy Iroegbu <mapt.dubem@gmail.com>
2023-07-19 20:09:20 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,32 @@
 import React from 'react';
-import landingPicture from '../assets/asset-01.jpg'
+import landingPicture from '../assets/asset-01.jpg';
+import habioLogo from '../assets/asset-02.png';
+import Person2OutlinedIcon from '@mui/icons-material/Person2Outlined';
 
 const Home = () => {
     return (
         <>
-            <div className="landingContainer">
-                <div className="landingImage">
-                    <img src={landingPicture} alt="landingImage" />
-                </div>
-                <div className="landingTextBox">
-                    <div className="brandNav">
-                        <h1 id="brand">habio</h1>
-                        <ul className="navContainer">
-                            <li className="nav"></li>
-                            <li className="nav"></li>
-                            <li className="nav"></li>
-                        </ul>
+            <header className="header">
+                <nav className="navBar">
+                    <div className="brand">
+                        <h2>HABIO</h2>
+                        <div className="empty"></div>
                     </div>
-                    <div className="companyInfo">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum eligendi enim eveniet qui iure sequi corporis voluptates iste autem nesciunt nisi velit suscipit, dolor magni! Veritatis quam ratione voluptas iste eaque aspernatur nostrum assumenda. Autem odio ut esse voluptatem. Tempore voluptas aperiam unde aspernatur a laborum in nisi repellendus laudantium?</p>
+                    <div className="habioLogo">
+                        <img src={habioLogo} alt="habioLogo" />
                     </div>
-                </div>
-            </div>
+                    <ul className="linkItems">
+                        <li className="linkItem">Home</li>
+                        <li className="linkItem">About</li>
+                        <li className="linkItem">Shop</li>
+                        <li className="linkItem">Cart</li>
+                        <li className="linkItem"><Person2OutlinedIcon /></li>
+                    </ul>
+                </nav>
+            </header>
+            <main className="main">
+                
+           </main>
         </>
     );
 };
