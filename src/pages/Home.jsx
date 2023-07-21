79f70d7b44fd606fcc67263c0c3751d0095d6324
Joy Iroegbu <mapt.dubem@gmail.com>
2023-07-21 13:32:01 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,25 +8,20 @@ const Home = () => {
         <>
             <header className="header">
                 <nav className="navBar">
-                    <div className="brand">
-                        <h2>HABIO</h2>
-                        <div className="empty"></div>
-                    </div>
-                    <div className="habioLogo">
-                        <img src={habioLogo} alt="habioLogo" />
-                    </div>
                     <ul className="linkItems">
-                        <li className="linkItem">Home</li>
-                        <li className="linkItem">About</li>
+                        <li className="linkItem"><img src={habioLogo} alt="habioLogo" /></li>
+                        <li className="linkItem"><a href="">Learn</a></li>
                         <li className="linkItem">Shop</li>
-                        <li className="linkItem">Cart</li>
+                    </ul>
+                    <ul className="linkItems">
+                        <li className="linkItem">Cart(0)</li>
                         <li className="linkItem"><Person2OutlinedIcon /></li>
                     </ul>
                 </nav>
             </header>
             <main className="main">
                 
-           </main>
+            </main>
         </>
     );
 };
